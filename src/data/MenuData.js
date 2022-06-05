b2015c17864dcd0fc8a5d3b2a03d82733ac4f2cc
nazmieievilya <ilyanazmeev111@gmail.com>
2022-06-05 15:44:59 +0300

    ss

--- src/data/MenuData.js
+++
@@ -1,7 +0,0 @@
-export const menuData = [
-    {title: "Home", link:"/#home" },
-    {title: "Works", link:"/#works" },
-    {title: "Testimonials", link:"/#testimonials" },
-    {title: "Contact", link:"/#contact" },
-
-]

eb4021e8a9c4555f5a2775e9ff54470bed610f52
nazmieievilya <ilyanazmeev111@gmail.com>
2022-06-06 14:23:19 +0300

    Service

--- src/pages/components/Service.js
+++ src/pages/components/Service.js
@@ -1,17 +1,116 @@
 import React from "react";
 import styled from "styled-components";
 const ServiceContainer = styled.div`
- 
   background-color: white;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 30vw;
+  @media (max-width: 768px) {
+    justify-content: center;
+    .serviceCont {
+      display: flex;
+      height: 10vw;
+      width: 10vw;
+      background-color: yellow;
+      margin: 10px;
+      
+    padding: 0px;
+    }
+  }
+  .serviceCont {
+    display: flex;
+    height: 10vw;
+    width: 20vw;
+    background-color: yellow;
+  }
+  h3 {
+    color: black;
+    font-size: 3vw;
+  }
+  p {
+    color: black;
+    font-size: 2vw;
+  }
 `;
 const Service = () => {
   return (
     <ServiceContainer>
-      <section className="py-5">
-        <div className="container">
-          <div className="col-12 col-md-6 p-4">
-            <div className="row">
-              <div className="col-12 col-md-2 text-primary">
+      <div className="serviceCont">
+        <div className="text-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="5vw"
+            height="5vw"
+            fill="currentColor"
+            class="bi bi-person-hearts"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M11.5 1.246c.832-.855 2.913.642 0 2.566-2.913-1.924-.832-3.421 0-2.566ZM9 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 8c0 1 1 1 1 1h10s1 0 1-1-1-4-6-4-6 3-6 4Zm13.5-8.09c1.387-1.425 4.855 1.07 0 4.277-4.854-3.207-1.387-5.702 0-4.276ZM15 2.165c.555-.57 1.942.428 0 1.711-1.942-1.283-.555-2.281 0-1.71Z"
+            />
+          </svg>
+        </div>
+        <div style={{ backgroundColor: "violet" }}>
+          <h3>Total transpatency</h3>
+          <p>lorem pipsum </p>
+        </div>
+      </div>
+      <div className="serviceCont">
+        <div className="text-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="5vw"
+            height="5vw"
+            fill="currentColor"
+            class="bi bi-person-hearts"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M11.5 1.246c.832-.855 2.913.642 0 2.566-2.913-1.924-.832-3.421 0-2.566ZM9 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 8c0 1 1 1 1 1h10s1 0 1-1-1-4-6-4-6 3-6 4Zm13.5-8.09c1.387-1.425 4.855 1.07 0 4.277-4.854-3.207-1.387-5.702 0-4.276ZM15 2.165c.555-.57 1.942.428 0 1.711-1.942-1.283-.555-2.281 0-1.71Z"
+            />
+          </svg>
+        </div>
+        <div style={{ backgroundColor: "violet" }}>
+          <h3>Total transpatency</h3>
+          <p>lorem pipsum </p>
+        </div>
+      </div>
+      <div className="serviceCont">
+        <div className="text-primary">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="5vw"
+            height="5vw"
+            fill="currentColor"
+            class="bi bi-person-hearts"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M11.5 1.246c.832-.855 2.913.642 0 2.566-2.913-1.924-.832-3.421 0-2.566ZM9 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 8c0 1 1 1 1 1h10s1 0 1-1-1-4-6-4-6 3-6 4Zm13.5-8.09c1.387-1.425 4.855 1.07 0 4.277-4.854-3.207-1.387-5.702 0-4.276ZM15 2.165c.555-.57 1.942.428 0 1.711-1.942-1.283-.555-2.281 0-1.71Z"
+            />
+          </svg>
+        </div>
+        <div style={{ backgroundColor: "violet" }}>
+          <h3>Total transpatency</h3>
+          <p>lorem pipsum </p>
+        </div>
+      </div>
+    </ServiceContainer>
+  );
+};
+
+export default Service;
+
+{
+  /* <div
+                
+                className="text-primary"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="30"
@@ -26,12 +125,8 @@ const Service = () => {
                   />
                 </svg>
               </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </ServiceContainer>
-  );
-};
-
-export default Service;
+              <div style={{ backgroundColor: "violet" }} >
+                  <h3>Total transpatency</h3>
+                  <p>lorem pipsum </p>
+                </div> */
+}

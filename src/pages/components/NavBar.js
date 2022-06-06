49177d1a8f609ad353b5a429f0a24158ac044c70
nazmieievilya <ilyanazmeev111@gmail.com>
2022-06-06 19:41:28 +0300

    NavBar interactive background

--- src/pages/components/NavBar.js
+++ src/pages/components/NavBar.js
@@ -1,6 +1,55 @@
 import React from "react";
 import styled from "styled-components";
+import { useEffect, useState } from "react";
+
 const NavBar = () => {
+  const [background, setBackground] = useState(false);
+  useEffect(() => {
+    const changeBackground = () => {
+      if (window.scrollY > 200) {
+        setBackground(true);
+      } else {
+        setBackground(false);
+      }
+    };
+    window.addEventListener("scroll", changeBackground);
+    return () => window.removeEventListener("scroll", changeBackground);
+  }, []);
+  console.log(background)
+  
+const NavContainer = styled.div`
+color: ${background ? "black" : "white" };
+background-color: ${background ? "white" : "transparant" };
+padding-right: 10px;
+padding-left: 10px;
+z-index: 50;
+top: 0;
+display: flex;
+justify-content: space-between;
+align-items: center;
+height: 40px;
+width: 100%;
+position: fixed;
+font-size: 2vw;
+.menu {
+  display: none;
+  visibility: hidden;
+}
+@media (max-width: 768px) {
+  .buttons {
+    display: none;
+    visibility: hidden;
+  }
+  .menu {
+    display: block;
+    visibility: visible;
+  }
+}
+
+@media (max-width: 600px) {
+  font-size: 3vw;
+}
+`;
   return (
     <NavContainer>
       <h5>something</h5>
@@ -32,39 +81,6 @@ const NavBar = () => {
   );
 };
 
-const NavContainer = styled.div`
-  color: black;
-  background-color: white;
-  padding-right: 10px;
-  padding-left: 10px;
-  z-index: 50;
-  top: 0;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 40px;
-  width: 100%;
-  position: fixed;
-  font-size: 2vw;
-  .menu {
-    display: none;
-    visibility: hidden;
-  }
-  @media (max-width: 768px) {
-    .buttons {
-      display: none;
-      visibility: hidden;
-    }
-    .menu {
-      display: block;
-      visibility: visible;
-    }
-  }
-
-  @media (max-width: 600px) {
-    font-size: 3vw;
-  }
-`;
 
 const List = styled.ul`
   height: 100%;

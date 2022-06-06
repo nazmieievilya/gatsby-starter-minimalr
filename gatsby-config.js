9dc59a461bf06fba30dd3d1ccf49acf9e341f597
nazmieievilya <ilyanazmeev111@gmail.com>
2022-06-06 15:42:30 +0300

    Styled

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {

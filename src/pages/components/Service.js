b244ea611c048347913189b1f0e79f85e3ded58b
nazmieievilya <ilyanazmeev111@gmail.com>
2022-06-06 19:33:47 +0300

    fixed services

--- src/pages/components/Service.js
+++ src/pages/components/Service.js
@@ -3,51 +3,62 @@ import styled from "styled-components";
 
 const ServiceContainer = styled.div`
   background-color: white;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  width: 100%;
-  height: 30vw;
-  
+  max-width: 100vw;
+  margin: 0 auto;
+  display: grid;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  gap: 1rem;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+}
+
   .serviceCont {
     display: flex;
-    height: 10vw;
-    width: 20vw;
-    
-
+    justify-content: center;
+    align-items: center;
+  }
+  svg {
+    width: 3vw;
+    height: 3vw;
+    margin: 10px;
   }
   h3 {
     padding: 0;
     color: black;
-    font-size: 3vw;
   }
   p {
     color: black;
-    font-size: 3vw;
   }
   @media (max-width: 768px) {
     padding: 0px;
     justify-content: center;
+    align-items: center;
+    grid-template-columns: repeat(2, 1fr);
     .serviceCont {
-      display: flex;
-      height: 20vw;
-      width: 40vw;
-      margin: 10px;
-
-      padding: 0px;
+      
+    }
+    svg {
+    width: 5vw;
+    height: 5vw;
+  }
+    h3 {
+      padding: 0;
+      color: black;
+      font-size: 3vw;
+    }
+    p {
+      color: black;
+      font-size: 2.5vw;
     }
   }
 `;
 const Service = () => {
   return (
-    
     <ServiceContainer>
       <div className="serviceCont">
         <div className="text-primary me-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="5vw"
-            height="5vw"
             fill="currentColor"
             class="bi bi-person-hearts"
             viewBox="0 0 16 16"
@@ -67,16 +78,11 @@ const Service = () => {
         <div className="text-primary me-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="5vw"
-            height="5vw"
             fill="currentColor"
-            class="bi bi-person-hearts"
+            class="bi bi-wallet2"
             viewBox="0 0 16 16"
           >
-            <path
-              fill-rule="evenodd"
-              d="M11.5 1.246c.832-.855 2.913.642 0 2.566-2.913-1.924-.832-3.421 0-2.566ZM9 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 8c0 1 1 1 1 1h10s1 0 1-1-1-4-6-4-6 3-6 4Zm13.5-8.09c1.387-1.425 4.855 1.07 0 4.277-4.854-3.207-1.387-5.702 0-4.276ZM15 2.165c.555-.57 1.942.428 0 1.711-1.942-1.283-.555-2.281 0-1.71Z"
-            />
+            <path d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499L12.136.326zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484L5.562 3zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z" />
           </svg>
         </div>
         <div>
@@ -88,16 +94,27 @@ const Service = () => {
         <div className="text-primary me-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="5vw"
-            height="5vw"
             fill="currentColor"
-            class="bi bi-person-hearts"
+            class="bi bi-book"
             viewBox="0 0 16 16"
           >
-            <path
-              fill-rule="evenodd"
-              d="M11.5 1.246c.832-.855 2.913.642 0 2.566-2.913-1.924-.832-3.421 0-2.566ZM9 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 8c0 1 1 1 1 1h10s1 0 1-1-1-4-6-4-6 3-6 4Zm13.5-8.09c1.387-1.425 4.855 1.07 0 4.277-4.854-3.207-1.387-5.702 0-4.276ZM15 2.165c.555-.57 1.942.428 0 1.711-1.942-1.283-.555-2.281 0-1.71Z"
-            />
+            <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
+          </svg>
+        </div>
+        <div>
+          <h3>Total transpatency</h3>
+          <p>lorem pipsum </p>
+        </div>
+      </div>
+      <div className="serviceCont">
+        <div className="text-primary me-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            class="bi bi-book"
+            viewBox="0 0 16 16"
+          >
+            <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811V2.828zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z" />
           </svg>
         </div>
         <div>
